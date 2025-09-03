e602463bd7f7040e189769a24f8d470ce1a388c3
benjamin <bl5572@pleasantonusd.net>
2025-09-03 16:29:40 -0700

    more optimizations, documentation, and removed unsafe serial commands

--- README.md
+++ README.md
@@ -3,4 +3,15 @@
 ### simple pomodoro timer for m5StickCplus2<br>
 
 ## planned changes:
-- nothing right now
+- Nothing right now
+
+
+## HOW TO USE:
+- On startup, the work timer will start as normal
+- Press BTN B during the work cycle to jump to break
+- Press BTN A during either work/break cycles to reset timer
+- Press BTN B during the break cycle to take off 1min from the break
+
+## Serial commands:
+- r: resetTimer
+- t: triggerBreak
--- src/main.cpp
+++ src/main.cpp
@@ -55,13 +55,8 @@ void loop() {
 
   if (Serial.available() > 0) {
     char c = Serial.read();
-    if (c == 'u') {
-      Serial.println("Recieved: increaseBrightness");
-      M5.Display.setBrightness(M5.Display.getBrightness() + 1);
-    } else if (c == 'd') {
-      Serial.println("Recieved: decreaseBrightness");
-      M5.Display.setBrightness(M5.Display.getBrightness() - 1);
-    } else if (c == 'r') {
+
+    if (c == 'r') {
       Serial.println("Recieved: resetTimer");
       resetTimer();
     } else if (c == 't') {
